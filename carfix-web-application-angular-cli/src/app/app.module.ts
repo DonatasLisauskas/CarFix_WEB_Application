c1061823057224c1ca6044c7bf8201c14501828f
Donatas Lisauskas <donliskas@gmail.com>
2020-11-18 01:15:33 +0200

    The app.module.ts and app.component.html files were filled by new components.

--- carfix-web-application-angular-cli/src/app/app.module.ts
+++ carfix-web-application-angular-cli/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {CustomersComponent} from './components/customers/customers.component';
+import {HttpClientModule} from '@angular/common/http';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import {RoutingModule} from './routing/routing.module';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent,
+    NavBarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RoutingModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
