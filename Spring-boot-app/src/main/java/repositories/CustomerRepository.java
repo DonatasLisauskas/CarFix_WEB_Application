b09d45ed2a203bbfc43f45a26c084da15a3ffba1
Donatas Lisauskas <donliskas@gmail.com>
2020-11-17 13:56:13 +0200

    The new repository interfaces are added.

---
+++ Spring-boot-app/src/main/java/repositories/CustomerRepository.java
@@ -0,0 +1,8 @@
+package repositories;
+
+import entities.Customer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long>{
+
+}
