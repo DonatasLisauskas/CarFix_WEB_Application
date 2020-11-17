b9c69f8d032968c3a2a2e0c7ebb22a72719bfeb8
Donatas Lisauskas <donliskas@gmail.com>
2020-11-17 14:13:05 +0200

    New spring boot project

---
+++ carfix-spring-boot/src/main/java/com/example/carfix/carfixspringboot/CarfixSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.carfix.carfixspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarfixSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarfixSpringBootApplication.class, args);
+    }
+
+}
---
+++ carfix-spring-boot/src/test/java/com/example/carfix/carfixspringboot/CarfixSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carfix.carfixspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarfixSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
