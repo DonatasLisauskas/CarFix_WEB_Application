810843dfd1ebce1d8ed9f6c7c0e613ebccbd99c6
Donatas Lisauskas <donliskas@gmail.com>
2020-11-19 16:30:19 +0200

    Spring security was added

--- carfix-spring-boot/src/main/java/com/example/carfix/carfixspringboot/repositories/CarRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.carfix.carfixspringboot.repositories;
-
-import com.example.carfix.carfixspringboot.entities.Car;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CarRepository extends CrudRepository<Car, Long> {
-}
--- carfix-spring-boot/src/main/java/com/example/carfix/carfixspringboot/repositories/CarServiceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.carfix.carfixspringboot.repositories;
-
-import com.example.carfix.carfixspringboot.entities.CarService;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CarServiceRepository extends CrudRepository<CarService, Long> {
-}
