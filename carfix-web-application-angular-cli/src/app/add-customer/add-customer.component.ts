a55be0036ccbdfdbabbdf1e7910ae39919c30e40
Donatas Lisauskas <donliskas@gmail.com>
2020-11-19 12:35:51 +0200

    The form is ended.

--- carfix-web-application-angular-cli/src/app/add-customer/add-customer.component.ts
+++ carfix-web-application-angular-cli/src/app/add-customer/add-customer.component.ts
@@ -12,45 +12,7 @@ import {ActivatedRoute, Router} from '@angular/router';
   styleUrls: ['./add-customer.component.css']
 })
 export class AddCustomerComponent implements OnInit {
-  postError = false;
-  postErrorMsg: string;
   customer: Customer = new Customer();
-  submitted = false;
-
-  constructor(private activatedRoute: ActivatedRoute,
-              private customerService: CustomerService,
-              private router: Router) {
-  }
-
-  ngOnInit(): void {
-  }
-  newCustomer(): void {
-    this.submitted = false;
-    this.customer = new Customer();
-  }
-
-  save(): void {
-    this.customerService.addNewCustomer(this.customer)
-      .subscribe(
-        result => console.log(`Success: `, result),
-        error => this.onHttpError(error),
-        () => console.log(`Completed!`));
-    this.customer = new Customer();
-  }
-
-  onSubmit(): void {
-    this.submitted = true;
-    this.save();
-    console.log(this.postErrorMsg);
-  }
-
-  private onHttpError(error: Error): void {
-    console.log(`Error: `, error);
-    this.postError = true;
-    console.log(this.postError);
-    this.postErrorMsg = error.message;
-  }
-/*  customer: Customer = new Customer();
   registerForm: FormGroup;
   submitted = false;
 
@@ -63,8 +25,8 @@ export class AddCustomerComponent implements OnInit {
       lastName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       phoneNumber: ['', [Validators.required, Validators.pattern('^(\\+\\d{1,3}-)?\\d{1,14}$')]],
-      /!*password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmPassword: ['', Validators.required]*!/
+      /*password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]*/
     }, {});
   }
 
@@ -76,34 +38,27 @@ export class AddCustomerComponent implements OnInit {
   onSubmit(): void {
     this.submitted = true;
     if (this.registerForm.invalid) {
-      // save data
-    }else {
-      this.submitted = false;
-      this.validateAllFields(this.registerForm);
-    }
-  }
-
-  validateAllFields(formGroup: FormGroup): void {
-    Object.keys(formGroup.controls).forEach(field => {
-      const control = formGroup.get(field);
-      if (control instanceof FormControl) {
-        control.markAsTouched({ onlySelf: true });
-      } else if (control instanceof FormGroup) {
-        this.validateAllFields(control);
-      }
-    });
-  }
-
- /!* onSubmit(): void {
-    this.submitted = true;
-    if (this.registerForm.invalid) {
       return;
-    }else {
-      this.router.navigate(['/customers/new']);
-      this.submitted = false;
-      localStorage.setItem('data', JSON.stringify(this.registerForm.value));
+    } else {
+      this.bindValues();
+      this.create();
     }
-  }*!/*/
+  }
+
+  bindValues(): void {
+    this.customer.firstName = this.registerForm.get('firstName').value;
+    this.customer.lastName = this.registerForm.get('lastName').value;
+    this.customer.email = this.registerForm.get('email').value;
+    this.customer.phoneNumber = this.registerForm.get('phoneNumber').value;
+  }
+
+  create(): void {
+    this.customerService.addNewCustomer(this.customer)
+      .subscribe(data => console.log(data), error => console.log(error));
+    this.customer = new Customer();
+  }
+
+
 
 }
 
