4fbcb6678d8faeff2496b16060752e01224f1ee8
Donatas Lisauskas <donliskas@gmail.com>
2020-11-25 21:04:37 +0200

    CarfixSpringBootApplication class was modified.

--- carfix-spring-boot/src/main/java/com/example/carfix/carfixspringboot/CarfixSpringBootApplication.java
+++ carfix-spring-boot/src/main/java/com/example/carfix/carfixspringboot/CarfixSpringBootApplication.java
@@ -1,13 +1,7 @@
 package com.example.carfix.carfixspringboot;
 
-import com.example.carfix.carfixspringboot.entities.Customer;
-import com.example.carfix.carfixspringboot.repositories.CustomerRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.stream.Stream;
 
 @SpringBootApplication
 public class CarfixSpringBootApplication {
@@ -15,16 +9,4 @@ public class CarfixSpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(CarfixSpringBootApplication.class, args);
     }
-
- /*   @Bean
-    CommandLineRunner init(CustomerRepository customers) {
-        return args -> {
-            Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
-                Customer customer = new Customer(name, name.toUpperCase(), name.toLowerCase() + "@domain.com", "+370-63800000");
-                customers.save(customer);
-            });
-            customers.findAll().forEach(System.out::println);
-
-        };
-    }*/
 }
