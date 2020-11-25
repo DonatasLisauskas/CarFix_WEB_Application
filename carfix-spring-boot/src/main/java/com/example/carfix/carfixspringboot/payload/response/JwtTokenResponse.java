369ba7355f9db5317c37e9f9a9e9f509ccea613d
Donatas Lisauskas <donliskas@gmail.com>
2020-11-25 21:01:20 +0200

    Response object was created.

---
+++ carfix-spring-boot/src/main/java/com/example/carfix/carfixspringboot/payload/response/JwtTokenResponse.java
@@ -0,0 +1,43 @@
+package com.example.carfix.carfixspringboot.payload.response;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class JwtTokenResponse {
+    
+    private String accessToken = "Bearer";
+
+    @NonNull
+    private String token;
+
+    @NonNull
+    private Long id;
+
+    @NonNull
+    private String firstname;
+
+    @NonNull
+    private String lastname;
+
+    @NonNull
+    private String username;
+
+    @NonNull
+    private String email;
+
+    @NonNull
+    private String phoneNumber;
+
+    @NonNull
+    private String password;
+
+    @NonNull
+    private List<String> role;
+}
