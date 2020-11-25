5112ba8e8aa72df67c71b131d1776f348d525eb4
Donatas Lisauskas <donliskas@gmail.com>
2020-11-25 21:01:45 +0200

    AuthenticationEntryPoint implementation was created.

---
+++ carfix-spring-boot/src/main/java/com/example/carfix/carfixspringboot/security/json_web_token_jwt/AuthenticationEntryPointJwt.java
@@ -0,0 +1,27 @@
+package com.example.carfix.carfixspringboot.security.json_web_token_jwt;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class AuthenticationEntryPointJwt implements AuthenticationEntryPoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthenticationEntryPointJwt.class);
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                         AuthenticationException authenticationException) throws IOException, ServletException {
+
+        logger.error("Unauthorized error: {}",authenticationException.getMessage());
+
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error 401: Unauthorized");
+    }
+}
