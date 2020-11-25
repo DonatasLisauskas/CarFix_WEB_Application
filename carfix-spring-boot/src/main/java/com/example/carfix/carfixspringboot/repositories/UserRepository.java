3519cf68b165a625b79490681c78c346149fe1ac
Donatas Lisauskas <donliskas@gmail.com>
2020-11-25 20:57:34 +0200

    User repository was created.

---
+++ carfix-spring-boot/src/main/java/com/example/carfix/carfixspringboot/repositories/UserRepository.java
@@ -0,0 +1,19 @@
+package com.example.carfix.carfixspringboot.repositories;
+
+import com.example.carfix.carfixspringboot.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+    Boolean existsByPhoneNumber(String phonenumber);
+
+}
