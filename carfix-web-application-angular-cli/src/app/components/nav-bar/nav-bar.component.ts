01be0f361fa4ba4fc2a4ca9c24f94e7b5358194b
Donatas Lisauskas <donliskas@gmail.com>
2020-11-26 20:15:04 +0200

    The new routs were added to the navbar.

--- carfix-web-application-angular-cli/src/app/components/nav-bar/nav-bar.component.ts
+++ carfix-web-application-angular-cli/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {TokenService} from '../../services/token/token.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,18 +7,36 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  private roles: string[];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username: string;
 
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(private tokenService: TokenService) { }
 
   ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenService.getToken();
+
+    if (this.isLoggedIn) {
+      const user = this.tokenService.getUser();
+      this.roles = user.roles;
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+      this.username = user.username;
+    }
   }
 
-  openNav(): void {
+  logout(): void {
+    this.tokenService.signOut();
+    window.location.reload();
+  }
+}
+
+ /* openNav(): void {
     document.getElementById('mySidenav').style.width = '15%';
   }
 
   closeNav(): void {
     document.getElementById('mySidenav').style.width = '0';
-  }
-
-}
+  }*/
--- carfix-web-application-angular-cli/src/app/routing/routing.module.ts
+++ carfix-web-application-angular-cli/src/app/routing/routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
-import {CustomersComponent} from '../components/customers/customers.component';
-import {AddCustomerComponent} from '../add-customer/add-customer.component';
+import {UserRegistrationComponent} from '../components/user_registration/user-registration.component';
+import {HomeComponent} from '../components/home/home/home.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'customers', pathMatch: 'full'},
-  {path: 'customers', component: CustomersComponent},
-  {path: 'customers/new', component: AddCustomerComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'registration', component: UserRegistrationComponent},
 
 ];
 
