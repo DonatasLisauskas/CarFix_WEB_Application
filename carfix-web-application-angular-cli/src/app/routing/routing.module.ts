d5a2b73cae8f904924bf526847db126821fdd89f
Donatas Lisauskas <donliskas@gmail.com>
2020-12-01 15:38:48 +0200

    "routing.module.ts" contains router paths that were populated by additional ones.

--- carfix-web-application-angular-cli/src/app/routing/routing.module.ts
+++ carfix-web-application-angular-cli/src/app/routing/routing.module.ts
@@ -1,13 +1,18 @@
-import { NgModule } from '@angular/core';
+import {NgModule, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {UserRegistrationComponent} from '../components/user_registration/user-registration.component';
-import {HomeComponent} from '../components/home/home/home.component';
+import {HomeComponent} from '../components/home/home.component';
+import {ProfileComponent} from '../components/profile/profile.component';
+import {UserLoginComponent} from '../components/user-login/user-login.component';
+import {TokenService} from '../services/token/token.service';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
+  {path: 'signIn', component: UserLoginComponent},
   {path: 'registration', component: UserRegistrationComponent},
+  {path: 'profile', component: ProfileComponent},
 
 ];
 
@@ -18,4 +23,5 @@ const routes: Routes = [
     CommonModule
   ]
 })
-export class RoutingModule { }
+export class RoutingModule {
+}
