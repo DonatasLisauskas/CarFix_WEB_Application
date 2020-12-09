ea9d72aeeb19af1a0f11643c4d9bd6df86e7a8dc
Donatas Lisauskas <donliskas@gmail.com>
2020-12-09 21:01:16 +0200

    New java persistence queries.

--- carfix-spring-boot/src/main/java/com/example/carfix/carfixspringboot/repositories/UserRepository.java
+++ carfix-spring-boot/src/main/java/com/example/carfix/carfixspringboot/repositories/UserRepository.java
@@ -10,6 +10,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
 
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByPhoneNumber(String phonenumber);
+
     Boolean existsByUsername(String username);
 
     Boolean existsByEmail(String email);
